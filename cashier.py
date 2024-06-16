e209f34cc5c874cd99b749b45b7d81f1c0d9a082
ThomasMcCauley <tmccaul4@uncc.edu>
2024-06-16 11:51:35 -0400

    implemented cashier.py by transfering functions from Assignment1

--- cashier.py
+++ cashier.py
@@ -5,9 +5,22 @@ class Cashier:
     def process_coins(self):
         """Returns the total calculated from coins inserted.
            Hint: include input() function here, e.g. input("how many quarters?: ")"""
-        ###
+        print("Please insert coins")
+        largeDollars = int(input("How many dollars? ")) * 1.00
+        halfDollars = int(input("How many half-dollars? ")) * .50
+        quarters = int(input("How many quarters? ")) * 0.25
+        nickles = int(input("How many nickles? ")) * 0.05
+        total = largeDollars + halfDollars + quarters + nickles
+        return total
 
     def transaction_result(self, coins, cost):
         """Return True when the payment is accepted, or False if money is insufficient.
            Hint: use the output of process_coins() function for cost input"""
-        ##
+        if coins >= cost:
+            change = coins - cost
+            if change > 0:
+                print(f"Here is ${change} in change.")
+            return True
+        else:
+            print("sorry, thats not enough money. Money refunded.")
+            return False
