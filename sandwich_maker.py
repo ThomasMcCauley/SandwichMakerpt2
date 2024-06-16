7600351754ad2e4bfbb391ff7bae3ff877d3550b
ThomasMcCauley <tmccaul4@uncc.edu>
2024-06-16 19:03:26 -0400

    implemented sandwich_maker.py by carrying over functions from Assignment1

--- sandwich_maker.py
+++ sandwich_maker.py
@@ -5,7 +5,15 @@ class SandwichMaker:
 
     def check_resources(self, ingredients):
         """Returns True when order can be made, False if ingredients are insufficient."""
-        #####
+        for ingredient, amount in ingredients.items():
+            if self.machine_resources[ingredient] < amount:
+                print("Sorry! there is not enough " + ingredient)
+                return False
+            return True
 
     def make_sandwich(self, sandwich_size, order_ingredients):
-        ########
+        """Deduct the required ingredients from the resources.
+           Hint: no output"""
+        for ingredient, amount in order_ingredients.items():
+            self.machine_resources[ingredient] -= amount
+        print(f" {sandwich_size} sandwich is ready. Bon appetit!")
